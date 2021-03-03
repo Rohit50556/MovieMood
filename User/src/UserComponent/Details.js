6b0ad973f8810eb7eb8e0b9bbdc05fdaffca3270
Rohit50556 <49819885+Rohit50556@users.noreply.github.com>
2021-03-03 23:36:53 +0530

    usercommit

---
+++ User/src/UserComponent/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Details = () =>{
+ return(<>
+    <h1>Details</h1>
+
+ </>
+ );
+}
+
+
+export default Details;
---
+++ User/src/UserComponent/History.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const History = () =>{
+ return(<>
+    <h1>History</h1>
+
+ </>
+ );
+}
+
+
+export default History;
